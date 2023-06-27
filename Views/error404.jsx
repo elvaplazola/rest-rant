9dd488ab022d0fc00b803d8deddea8fa24188537
Elva Plazola <elvaplazola@yahoo.com>
2023-06-27 03:50:41 -0700

    Adding Bootsrap grid to index page

--- Views/error404.jsx
+++ Views/error404.jsx
@@ -7,6 +7,12 @@ function error404 () {
          <main>
              <h1>404: PAGE NOT FOUND</h1>
              <p>Oops, sorry, we can't find this page!</p>
+             <div>
+                <img src= "/images/sad_pug.jpg" id = "sad_pug" alt = "sad_pug"></img>
+                <div>
+                Photo by <a href="https://unsplash.com/fr/@matthewhenry?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Matthew Henry</a> on <a href="https://unsplash.com/s/photos/funny-animal?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+             </div>
         </main>
         </Def>
     )
--- Views/home.jsx
+++ Views/home.jsx
@@ -6,6 +6,12 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/brazilian_steakhouse.jpg" id="brazilian_steakhouse" alt="Brazilian Steakhouse"></img>
+                <div>
+                Photo by <a href="https://unsplash.com/@victoriakosmo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Victoria Shes</a> on <a href="https://unsplash.com/images/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+                </div>
+              </div>
               <a href="/places">
                 <button className="btn-primary">Places Page</button>
               </a>
