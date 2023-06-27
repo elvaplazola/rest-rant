b30494fac7c57a8cd27cd83b10d91892d6e28f61
Elva Plazola <elvaplazola@yahoo.com>
2023-06-27 05:09:59 -0700

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/dark_dining.jpg'
+}, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/fine_dining.jpg'
+}]
